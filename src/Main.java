19a8f93c8b61cb9fbdc02ba756aa3578a427e8f7
lmfreire <l.matheus.freire10@gmil.com>
2023-09-19 22:25:57 -0300

    inicio da modelagem de curso/mentoria

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso Java");
+        curso1.setDescricao("descrição curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso JS");
+        curso2.setDescricao("descrição curso JS");
+        curso2.setCargaHoraria(8);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("mentoria Java");
+        mentoria1.setDescricao("descrição mentoria Java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(mentoria1);
+    }
+}
